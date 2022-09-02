e7a9551bc8422d47d106da658b09f5d18b35253f
Yuvam <67573209+yuvam2017@users.noreply.github.com>
2022-09-02 11:36:51 +0530

    Update yaru.sh

--- yaru.sh
+++ yaru.sh
@@ -5,3 +5,4 @@ sudo apt upgrade
 notify-send "Upgraded !!" "System is now upgraded"
 sudo apt install gnome-tweaks
 sudo apt install yaru-theme-gtk yaru-theme-sound yaru-theme-gnome-shell yaru-theme-icon yaru-theme-unity
+sudo reboot
